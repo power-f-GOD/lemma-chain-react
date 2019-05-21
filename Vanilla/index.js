449b9f7a8e21c46179bb1605aa04f2b37b86a3e4
powerofgod <sundaypower9@gmail.com>
2019-05-21 23:08:23 +0100

    Made some modifications and fixed some bugs. PS: Still work in progress...

--- Vanilla/index.js
+++ Vanilla/index.js
@@ -4,15 +4,16 @@ let Q = document.querySelector.bind(document),
     QAll = document.querySelectorAll.bind(document);
 
 
+
 //load script when DOM is ready
 this.addEventListener('DOMContentLoaded', function()
 {
-  let req = Q('.recommended-dropdown'),
-      rec = Q('.required-dropdown');
+  let rec = Q('.recommended-dropdown'),
+      req = Q('.required-dropdown');
 
 
   //toggle between hiding and showing dropdown menu
-  Q('.ref-dropdown-wrapper').onclick = function()
+  Q('.ref-dropdown-wrapper').addEventListener('click', function()
   {
     Q('.dropdown-container').classList.toggle('hide');
     Q('.ref-dropdown-icon').classList.toggle('icon-flip');
@@ -21,51 +22,44 @@ this.addEventListener('DOMContentLoaded', function()
       resizeDropdownHeight(0);
     else
       resizeDropdownHeight([Q('.active-dropdown')]);
-  };
-
-
-  Q('.required-btn').onclick = function()
-  {
-    let req = Q('.recommended-dropdown'),
-      rec = Q('.required-dropdown');
-    this.classList.add('active');
-    this.nextElementSibling.classList.remove('active');
-    rec.classList.splice(rec.classList.indexOf('active-dropdown'), 1, 'inactive-dropdown');
-    req.classList.splice(req.classList.indexOf('inactive-dropdown'), 1, 'active-dropdown');
-    resizeDropdownHeight([Q('.required-dropdown')]);
-  }
+  });
 
 
   //display required references
-  Q('.required-btn').onclick = function()
-  {
-    this.classList.add('active');
-    this.nextElementSibling.classList.remove('active');
-    Q('.recommended-dropdown').classList.remove('active-dropdown');
-    Q('.recommended-dropdown').classList.add('inactive-dropdown');
-    Q('.required-dropdown').classList.remove('inactive-dropdown');
-    Q('.required-dropdown').classList.add('active-dropdown');
-    resizeDropdownHeight([Q('.required-dropdown')]);
-  }
+  Q('.required-btn').addEventListener('click', function()
+  { toggleReferences(this, this.nextElementSibling, req, rec); });
 
 
   //display recommended references
-  Q('.recommended-btn').onclick = function()
+  Q('.recommended-btn').addEventListener('click', function()
+  { toggleReferences(this.previousElementSibling, this, rec, req); });
+
+
+  // references toggler
+  function toggleReferences(btn0, btn1, activDrop, inactivDrop)
   {
-    this.classList.add('active');
-    this.previousElementSibling.classList.remove('active');
-    Q('.required-dropdown').classList.remove('active-dropdown');
-    Q('.required-dropdown').classList.add('inactive-dropdown');
-    Q('.recommended-dropdown').classList.remove('inactive-dropdown');
-    Q('.recommended-dropdown').classList.add('active-dropdown');
-    resizeDropdownHeight([Q('.recommended-dropdown')]);
+    if (/act/.test(btn0.className))
+      btn0.classList.remove('active'),
+      btn1.classList.add('active'),
+      btn0.innerHTML = 'Required',
+      btn1.innerHTML = '› Recommended ‹';
+    else
+      btn0.classList.add('active'),
+      btn1.classList.remove('active'),
+      btn0.innerHTML = '› Required ‹',
+      btn1.innerHTML = 'Recommended';
+    
+    inactivDrop.classList.remove('active-dropdown');
+    inactivDrop.classList.add('inactive-dropdown');
+    activDrop.classList.remove('inactive-dropdown');
+    activDrop.classList.add('active-dropdown');
+    resizeDropdownHeight([activDrop]);
   }
 
 
   function resizeDropdownHeight(refs, cons = Q('.top-toggle-wrapper'))
   {
-    let height = refs.reduce ? refs.concat(cons).reduce((a, b) => a.offsetHeight + b.offsetHeight) : 0;
-
+    let height = refs.concat ? refs.concat(cons).reduce((a, b) => a.offsetHeight + b.offsetHeight) : 0;
     Q('.dropdown-container').style.height = `${height}px`;
   }
 });
