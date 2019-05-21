eb2d6e1eba77b9da7a53ac117429ba29ce1978a3
Ikechukwu Eze <iykekings36@gmail.com>
2019-05-21 23:15:22 +0100

    Implement logic in JS

---
+++ Vanilla/main.js
@@ -0,0 +1,42 @@
+class TabLink {
+  constructor(element) {
+    this.element = element;
+    this.toggle = document.querySelector('.top-toggle');
+    this.dataset = element.dataset.tab;
+    this.tabContentElem = document.querySelector(
+      `.tab-content[data-tab="${this.dataset}"]`
+    );
+    this.tabContent = new TabContent(this.tabContentElem);
+
+    // btns
+    this.backBtn = element.querySelector('.back-btn.svg-icon');
+    this.starBtn = element.querySelector('.star-btn.svg-icon');
+
+    //Events
+    this.element.addEventListener('click', this.select.bind(this));
+    this.toggle.addEventListener('click', this.activate);
+  }
+  activate() {
+    const body = document.querySelector('.ref_body');
+    body.classList.toggle('active');
+  }
+  select() {
+    const tabs = document.querySelectorAll('.tablink');
+    tabs.forEach(tab => tab.classList.remove('active'));
+    this.element.classList.add('active');
+    this.tabContent.select();
+  }
+}
+
+class TabContent {
+  constructor(element) {
+    this.element = element;
+  }
+  select() {
+    const tabCons = document.querySelectorAll('.tab-content');
+    tabCons.forEach(tabCon => tabCon.classList.remove('active'));
+    this.element.classList.add('active');
+  }
+}
+const tabs = document.querySelectorAll('.tablink');
+tabs.forEach(tab => new TabLink(tab));
