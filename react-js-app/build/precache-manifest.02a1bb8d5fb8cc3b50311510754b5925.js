1a0df69befec248536b64aaa7424071d6ade36b4
powerofgod <sundaypower9@gmail.com>
2019-06-09 00:24:50 +0100

    Modified inactive tab link background color and styled scroll bar

---
+++ react-js-app/build/precache-manifest.02a1bb8d5fb8cc3b50311510754b5925.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0f161ce50a7e3a70d90b33ef62ab2ec",
+    "url": "/lemma-chain-react/react-js-app/index.html"
+  },
+  {
+    "revision": "711ec6c5a37b8c8b5048",
+    "url": "/lemma-chain-react/react-js-app/static/css/main.cc9d2881.chunk.css"
+  },
+  {
+    "revision": "bc885b2d6f1dcbc36268",
+    "url": "/lemma-chain-react/react-js-app/static/js/2.45b30e71.chunk.js"
+  },
+  {
+    "revision": "711ec6c5a37b8c8b5048",
+    "url": "/lemma-chain-react/react-js-app/static/js/main.fc61b190.chunk.js"
+  },
+  {
+    "revision": "29733a946fdc646d17bb",
+    "url": "/lemma-chain-react/react-js-app/static/js/runtime~main.fbfa5f4e.js"
+  }
+]);
