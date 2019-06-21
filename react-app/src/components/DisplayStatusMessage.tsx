d6ba86112b523713e7a8ebbc73a217e98d8c98e4
powerofgod <sundaypower9@gmail.com>
2019-06-21 11:35:37 +0100

    Finalized GUI; fixed bugs. To-Do: Implement graph visualization (Staged for the coming week).

--- react-app/src/components/DisplayStatusMessage.tsx
+++ react-app/src/components/DisplayStatusMessage.tsx
@@ -8,48 +8,48 @@ import React, { CSSProperties } from 'react';
 export default function DisplayStatusMessage(props: any)
 {
   let messageWrapperStyle: CSSProperties = 
-    {
-      padding: '20px 30px',
-      display: 'flex',
-      textAlign: 'center',
-      justifyContent: 'center',
-      alignItems: 'center',
-      color: '#888',
-      fontStyle: 'italic',
-      fontSize: 14,
-      minHeight: 'auto',
-      height: 260
-    },
-    errImgStyle: CSSProperties = 
-    {
-      opacity: 0.5,
-      borderRadius: '50%',
-      width: 80,
-      height: 'auto',
-      marginBottom: 10
-    },
-    suggestMsg: React.ReactFragment =
-      <><b>Suggest:</b> Must be a network issue. Check internet connection then try again.</>,
-    errMsg: React.ReactFragment = 
-      <>
-        Sorry. Could not load <b>{props.for_ref_type}</b> references for this book.<br /><b>ErrorMessage:</b> {props.errMsg}<br /><br />
-        {/fail|fetch|error/i.test(props.errMsg) ? suggestMsg : ''}
-      </>,
-    nothingToShowMessage: React.ReactFragment =
-      <>Nothing to show.<br />Book has no <b>{props.for_ref_type}</b> references.</>,
-    graphStatusMessage: React.ReactFragment =
-      <>
-        Sorry. Can't visualize <b>graph</b>.<br />
-        <b>{props.errMsg ? 'ErrorMessage:' : 'Reason:'}</b> {props.errMsg || 'Book has no parent refs.'}
-      </>;
+      {
+        padding: '20px 30px',
+        display: 'flex',
+        textAlign: 'center',
+        justifyContent: 'center',
+        alignItems: 'center',
+        color: '#888',
+        fontStyle: 'italic',
+        fontSize: 14,
+        minHeight: 'auto',
+        height: 260
+      },
+      errImgStyle: CSSProperties = 
+      {
+        opacity: 0.5,
+        borderRadius: '50%',
+        width: 80,
+        height: 'auto',
+        marginBottom: 10
+      },
+      suggestMsg: React.ReactFragment =
+        <><b>Suggest:</b> Must be a network issue. Check internet connection then try again.</>,
+      errMsg: React.ReactFragment = 
+        <>
+          Sorry. Could not load <b>{props.ref_type}</b> references for this book.<br /><b>ErrorMessage:</b> {props.errMsg}<br /><br />
+          {/fail|fetch|error/i.test(props.errMsg) ? suggestMsg : ''}
+        </>,
+      nothingToShowMessage: React.ReactFragment =
+        <>Nothing to show.<br />Book has no <b>{props.ref_type}</b> references.</>,
+      graphStatusMessage: React.ReactFragment =
+        <>
+          Sorry. Can't visualize <b>graph</b>.<br />
+          <b>{props.errMsg ? 'ErrorMessage:' : 'Reason:'}</b> {props.errMsg || 'Book has no parent refs.'}
+        </>;
 
   return (
     <div className='item-wrapper' style={messageWrapperStyle}>
       <span className='props'>
         <img src='err_grey.png' alt='Status' style={errImgStyle}/><br />
         {
-          /graph/.test(props.for_ref_type) ? 
-            graphStatusMessage : (/error/.test(props.type) ? errMsg : nothingToShowMessage)
+          /graph/.test(props.ref_type) ? 
+            graphStatusMessage : (/error/.test(props.typeofMsg) ? errMsg : nothingToShowMessage)
         }
       </span>
     </div>
